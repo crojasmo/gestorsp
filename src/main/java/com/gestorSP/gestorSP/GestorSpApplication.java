64acda52ffef695d4920483edeff0b78dd936f64
Carlos Eugenio Rojas Morales <carlos.rojasmo@sansano.usm.cl>
2020-07-20 16:19:54 -0400

    Primer pool

---
+++ src/main/java/com/gestorSP/gestorSP/GestorSpApplication.java
@@ -0,0 +1,13 @@
+package com.gestorSP.gestorSP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorSpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorSpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestorSP/gestorSP/GestorSpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestorSP.gestorSP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorSpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
