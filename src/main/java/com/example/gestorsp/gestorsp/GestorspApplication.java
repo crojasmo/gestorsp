3b2bbb3fa4185935f40c7a11dbdab6b4475f5d01
Carlos Eugenio Rojas Morales <carlos.rojasmo@sansano.usm.cl>
2020-07-20 16:31:19 -0400

    Nuevo proyecto

---
+++ src/main/java/com/example/gestorsp/gestorsp/GestorspApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestorsp.gestorsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorspApplication.class, args);
+	}
+
+}
