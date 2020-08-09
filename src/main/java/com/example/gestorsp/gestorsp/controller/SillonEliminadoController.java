7cb6637aa9c2b2446a729cdd7c66270e20d21ee4
Carlos Eugenio Rojas Morales <carlos.rojasmo@sansano.usm.cl>
2020-08-09 17:41:45 -0400

    devolver

--- src/main/java/com/example/gestorsp/gestorsp/controller/SillonEliminadoController.java
+++ src/main/java/com/example/gestorsp/gestorsp/controller/SillonEliminadoController.java
@@ -1,13 +1,15 @@
 package com.example.gestorsp.gestorsp.controller;
 
+import com.example.gestorsp.gestorsp.models.Sillon;
 import com.example.gestorsp.gestorsp.models.SillonEliminado;
 import com.example.gestorsp.gestorsp.repository.SillonEliminadoRepository;
+import com.example.gestorsp.gestorsp.repository.SillonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
+import java.util.Date;
 
 import com.example.gestorsp.gestorsp.exceptions.*;
 
@@ -18,6 +20,7 @@ import com.example.gestorsp.gestorsp.exceptions.*;
 public class SillonEliminadoController {
     @Autowired
     private SillonEliminadoRepository sillonEliminadoRepository;
+    private SillonRepository sillonRepository;
     @GetMapping("/silloneseliminados")
     public Iterable<SillonEliminado> getSillonEliminado(Pageable pageable){
         return sillonEliminadoRepository.findAll();
@@ -29,5 +32,18 @@ public class SillonEliminadoController {
             return sillonEliminadoRepository.save(sillon);
         }).orElseThrow(()-> new ResourceNotFoundException("Sillon Eliminado not found with id " + sillonId));
     }
+    @DeleteMapping("silloneseliminados/{sillonId}/devolver")
+    public Sillon sillonRevive(@PathVariable Long sillonId){
+        if (sillonRepository.findById(sillonId).get().getActivo()==false){
+            throw new DeletedException("Sillon con la id: "+ sillonId +" no esta eliminado");
+        }
+        return sillonRepository.findById(sillonId)
+            .map(sillon -> {
+                sillon.setActivo(true);
+                Date fecha_hora=new Date();
+                sillon.setFecha_update(fecha_hora);
+                return sillonRepository.save(sillon);
+            }).orElseThrow(() -> new ResourceNotFoundException("Sillon no encontrado con id: " + sillonId));
+    }
+    }
 
-}
